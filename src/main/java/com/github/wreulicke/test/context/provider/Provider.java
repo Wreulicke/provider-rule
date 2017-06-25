11a8e0175c2ee035f82121c275dc7d07e9a575c4
wreulicke <saito.masaya@classmethod.jp>
2017-06-26 03:10:34 +0900

    add javadoc

--- src/main/java/com/github/wreulicke/test/context/provider/Provider.java
+++ src/main/java/com/github/wreulicke/test/context/provider/Provider.java
@@ -3,6 +3,13 @@ package com.github.wreulicke.test.context.provider;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+/**
+ * Implementation must provide no-args constructor.
+ * 
+ * @author wreulicke
+ * @since 0.0.1
+ *
+ */
 public interface Provider {
 	void visitMethod(Object instance, Method method) throws Exception;
 	
