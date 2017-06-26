8de0d993c3153958770f6550695809cc833c0f19
wreulicke <saito.masaya@classmethod.jp>
2017-06-27 00:59:33 +0900

    [WIP] migrate TypedProvider

---
+++ src/main/java/com/github/wreulicke/test/context/provider/AbstractTypedProvider.java
@@ -0,0 +1,6 @@
+package com.github.wreulicke.test.context.provider;
+
+import java.lang.annotation.Annotation;
+
+public abstract class AbstractTypedProvider<T extends Annotation> implements Provider {
+}
